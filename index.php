a24badbd6666f1def628689ed1d5820d56486a73
Svend Hansen <github@sentenceman.com>
2013-08-27 11:23:17 +0100

    Added initial index.php file.

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Marianne Hesselholt - Forfatter og Foredragsholder.</title>
+</head>
+<body>
+	<h1>Header</h1>
+	<h2>Sub Header</h2>
+	<div>
+		Her er en masse text some fylder en lille smule mere. Jeg kunne maaske have kopieret dette fra et sted paa internettet, men nu er jeg begyndt at skrive noget, og det er ogsaa i virkeligheden sjovere, selvom det tager noget laengere tid...
+	</div>
+</body>
+</html>
