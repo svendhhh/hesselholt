1f1748b7613732f654474021424e2ad95ade5960
Svend Hansen (home) <github@sentenceman.com>
2013-09-01 21:47:47 +0100

    Added header image, navigation, mugshot and lots of other stuff to get started.

--- index.php
+++ index.php
@@ -3,12 +3,44 @@
 <head>
 	<title>Marianne Hesselholt - Forfatter og Foredragsholder.</title>
 	<script type="text/javascript" src="js/jquery.js" ></script>
+	<script type="text/javascript" src="js/global.js" ></script>
+	<link href="css/index.css" type="text/css" rel="stylesheet"></link>
+	<link href="css/global.css" type="text/css" rel="stylesheet"></link>
 </head>
 <body>
-	<h1>Header</h1>
-	<h2>Sub Header</h2>
-	<div>
-		Her er en masse text some fylder en lille smule mere. Jeg kunne maaske have kopieret dette fra et sted paa internettet, men nu er jeg begyndt at skrive noget, og det er ogsaa i virkeligheden sjovere, selvom det tager noget laengere tid...
-	</div>
+  <div id="content">
+    <div id="headerImg"></div>
+    <div id="headerNav">
+      <ul id="nav">
+	<a href="index.php"><li>Hjem</li></a>
+	<a href="about.php"><li>Om Marianne</li></a>
+	<a href="books.php"><li>Mariannes bøger</li></a>
+	<a href="talks.php"><li>Foredrag</li></a>
+	<a href="contact.php"><li>Kontakt</li></a>
+      </ul>
+    </div>
+    <div id="body">
+
+      <div id="mugshot"></div>
+      <div id="intro" class="text">
+	Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! 
+      </div>
+      <div id="featuredbook">
+	Featured book goes here
+      </div>
+      <div id="latestblog">
+	Latest blog post goes here
+      </div>
+      <div id="nexttalk">
+	Next talk goes here
+      </div>
+      <div id="externallinks">
+	External links go here
+      </div>
+    </div>
+    <div id="footer">
+
+    </div>
+</div>
 </body>
 </html>
