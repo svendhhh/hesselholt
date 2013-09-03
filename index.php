e954e9ac6e6f6e469ad19acee20000cb02d9604a
Svend Hansen (home) <github@sentenceman.com>
2013-09-03 22:19:44 +0100

    Added more stuff (featured book, coming talks and more).

--- index.php
+++ index.php
@@ -25,15 +25,43 @@
       <div id="intro" class="text">
 	Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! Welcome to my website! 
       </div>
-      <div id="featuredbook">
-	Featured book goes here
-      </div>
-      <div id="latestblog">
-	Latest blog post goes here
-      </div>
-      <div id="nexttalk">
-	Next talk goes here
-      </div>
+      <table class="skinnytable">
+	<tr>
+	  <td id="featuredbook">
+	    <div id="featuredbookimg"></div>
+	    <div id="featuredbooktxt">
+	      <span class="title">
+		Digteren og Malerinden
+	      </span>
+	      I romanen vil man møde to kunstnere der på mange måder skilte sig ud fra deres samtid, men som skabte verdener af stor skønhed.
+	      <button id="readmore">Læs mere</button>
+	    </div>
+	  </td>
+	  <td id="latestblog" rowspan="2">
+	    Latest blog post goes here
+	  </td>
+	</tr>
+	<tr>
+	  <td id="nexttalk" valign="top">
+	    <span class="title">
+	      Kommende foredrag
+	    </span>
+	    <?php
+	       $today = getdate();
+	       $lines = file("2013.txt", FILE_SKIP_EMPTY_LINES);
+	       $count = 0;
+	       foreach($lines as &$line) {
+	       if ($count > 3) { break; }
+	    $parts = explode(",", $line);
+	    $line_date = date_parse($parts[0]);
+	    if ($line_date['month'] < $today['mon'] || ($line_date['month'] == $today['mon'] && $line_date['day'] < $today['mday'])) { continue; }
+	    print($line."<br>");
+	    $count++;
+	    }
+	    ?>
+	  </td>
+	</tr>
+      </table>
       <div id="externallinks">
 	External links go here
       </div>
