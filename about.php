2c4c4ae5a3e41bd90e68164b4d14eda124d33472
Svend Hansen (home) <github@sentenceman.com>
2013-10-03 22:30:30 +0100

    Filled in the rest of the about page.

--- about.php
+++ about.php
@@ -18,30 +18,61 @@ require 'macros.php';
       <?php insert_header(2); ?>
       <div id="mugshot"></div>
       <div class="content-frame text">
-	<span class="title">Lidt om forfatteren:</span>
+	<span class="title">Lidt om forfatteren</span>
 	<p>
+	  <img style="float:left" src="img/lille_janne.jpg"></img>
 	  Marianne Hesselholt er født 7.9 1943 i den lille nordjyske fiskerby Strandby, hvor faderen Magnus Hesselholt var auktionsmester, mens moderen, Vera Hesselholt, var hjemmegående og tog sig af husholdningen og de 5 børn. Som 9årig kom hun på Freys Skole i Frederikshavn og tog herfra realeksamen.
 	</p>
+	<hr style="clear:both" />
+	<img style="float:left" src="img/marianne_15_aar.jpg"></img>
 	<p>
-	  Født 7.9 1943 i Strandby ved Frederikshavn.
+	  Herefter tilbragte hun 3 år på Th. Langs Gymnasium i Silkeborg, hvor hun tog studentereksamen i 1962. Derpå gik turen til Paris, og her tilbragte hun nogle måneder på en sprogskole, der var ledet af Simon Spies.
+	</p>
+	<p>
+	  Læste i 1963 til korrespondent på Handelshøjskolen i Ålborg. Gik senere i gang med at studere fransk og dansk på Århus Universitet. Blev cand.mag. i fransk og dansk i 1974. 
+	</p>
+
+	<hr style="clear:both" />
+
+	<img style="float:left" src="img/landbrugets_pris.jpg"></img>
+	<p>
+	  Virkede som Lærer på Randers Handelsskole fra 1967 til 1976 og på Randers Amtsgymnasium fra 1967 til 1992. Underviste på Silkeborg Amtsgymnasium fra 1992 til 1996.
 	</p>
 
 	<p>
-    Læste først på Handelshøjskolen i Ålborg, senere på Århus Universitet.
-    Cand mag. i fransk og dansk 1975.
+	  Debut som forfatter i 1988 med Fussingøspillet: Og Lyset frembryder. Modtog i den anledning en pris fra Landboforeningen.
 	</p>
 
 	<p>
-    Lærer på Randers Handelsskole fra 1967 til 1976. På Randers Amtsgymnasium fra 1967 til 1992 og på Silkeborg Amtsgymnasium fra 1992 til 1996.
+	  Udgav den første roman i 1993 og har herefter fungeret som fuldtidsforfatter og foredragsholder.
+	</p>
+
+	<hr style="clear:both" />
+
+	<span class="title">
+	  Det private liv
+	</span>
+
+	<img style="float:left" src="img/bryllup_1967.jpg"></img>
+	<p>
+	  Gift med Ole Henne Hansen i 1967. De har sammen tre børn: Hans fra 1968, Mette fra 1972 og Svend fra 1981.
+	</p>
+
+	<hr style="clear:both">
+
+	<img style="float:left" src="img/hans.jpg"></img>
+	<img style="float:left" src="img/mette.jpg"></img>
+	<img style="float:left" src="img/svend_og_katie.jpg"></img>
+
+	<hr style="clear:both">
+
+	<p>
 	</p>
 
 	<p>
-    Udgav den første roman i 1993 og lever fra 1996 som fuldtidsforfatter og foredragsholder.
 	</p>
 
 	<p>
-    Bor i Landsbyen Ålum ved Randers. Gift med Ole Henne Hansen.
-    Har sammen tre børn: Hans fra 1968, Mette fra 1972 og Svend fra 1981. 
 	</p>
 
       </div>
