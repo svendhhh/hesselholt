8f3ec687cd7801aa855b9fb2fe2c5f5f5e392955
Svend Hansen (home) <github@sentenceman.com>
2013-12-02 19:22:03 +0000

    Added buying instructions.

---
+++ buy-a-book.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<?php
+require 'macros.php';
+?>
+
+<html>
+<?php macro_open_head(); ?>
+</head>
+<body>
+  <div id="content">
+    <div id="body">
+      <?php insert_header(3); ?>
+      <div id="mugshot"></div>
+      <div class="content-frame text">
+
+	<div class="text">Sådan køber du Mariannes bøger.</div>
+
+	<div class="text">
+	  Hos boghandleren koster bøgerne stadig fra 199 kr. til 299. kr.<br />
+	  Hos forfatteren kan du købe dem til <b>blot 120 kr.</b> og få dem tilsendt <b>portofrit</b>. <br />
+	  Bestiller man over 4 bøger ( f.eks. klassesæt ) koster det dog porto.<br />
+	  Du skal blot skrive din adresse til mig - så sender jeg bogen sammen med en faktura.
+	</div>
+
+      </div>
+    </div>
+  </div>
+</body>
+
+
+</html>
