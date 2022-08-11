cb0304bbd47705b2e35224d46d28094166511d09
Svend Hansen <svendhhh@gmail.com>
2022-08-11 14:46:31 +0100

    Added talk in Veng, removed old talk topics and added talk topic about Dronning Elisabeth.

--- about-talks.php
+++ about-talks.php
@@ -25,13 +25,8 @@ if (isset($_GET["talk"])) {
 	   ?>
 	Jeg kommer i hele landet med f&oslash;lgende foredrag:
 	<ul>
-	  <a href="about-talks.php?talk=kingo"><li>Thomas Kingo.</li></a>
-	  <a href="about-talks.php?talk=brorson"><li>Hans Adolph Brorson.</li></a>
-	  <a href="about-talks.php?talk=ingemann"><li>Bernhard S. Ingemann.</li></a>
-	  <a href="about-talks.php?talk=lucie"><li>Lucie Mandix. En varm kvinde i en kold tid.</li></a>
-	  <a href="about-talks.php?talk=hans-fugl"><li>Hans Fugl. En udvandrerkrønike.</li></a>
 	  <a href="about-talks.php?talk=hjemstavn-og-udsyn"><li>Hjemstavn og Udsyn.</li></a>
-	  <a href="about-talks.php?talk=engle"><li>Engle, milde eller vilde?</li></a>
+	  <a href="about-talks.php?talk=dronning-elisabeth"><li>Dronning Elisabeth af Kandestederne</li></a>
 	  <a href="about-talks.php?talk=skyggen-af-en-hverdag"><li>Skyggen af en Hverdag (om Alzheimer).</li></a>
 	</ul>
 	<p>Pris for foredrag: 3000,- kr. plus transport.</p>
